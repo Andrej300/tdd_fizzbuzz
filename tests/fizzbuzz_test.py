c777630a1d98a24beebccc55e97728333a454aff
Andrej Mihalylovics <andrej.mihalylovics@gmail.com>
2020-11-26 12:55:09 +0000

    initial commit

---
+++ tests/fizzbuzz_test.py
@@ -0,0 +1,7 @@
+import unittest
+from src.fizzbuzz import fizzbuzz
+
+class TestFizzBuzz(unittest.TestCase):
+
+    def test_fizzbuzz_3_return_fizz(self):
+        self.assertEqual("fizz", fizzbuzz(3))
